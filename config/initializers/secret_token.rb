4b9ccfcc130bebf8d4a25329d1c317f94a3df817
Birte <birte@geekmail.de>
2013-10-12 14:18:02 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RentARoleModel::Application.config.secret_key_base = '51975cd58966d315aaa6c0aaa184f6a693af0cf83abd3a0d511a10c98bbdff2222f8d8c34e3ef702e8d9e060ce1073e89be5d04bc24da009caaa186bafd173b8'
