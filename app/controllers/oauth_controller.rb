1a55fcf09b6d1dc2e324f28fea2984878869d13e
Birte <birte@geekmail.de>
2013-11-16 11:43:24 +0100

    differerenciate callback_url by environment

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,7 +3,7 @@ class OauthController < ApplicationController
   before_filter :set_consumer
 
   def sign_up
-    request_token = @consumer.get_request_token(:oauth_callback =>"http://rent-a-role-model.herokuapp.com/callback")
+    request_token = @consumer.get_request_token(:oauth_callback => callback_url)
     session[:token] = request_token.token
     session[:secret] = request_token.secret
     # print request_token
@@ -38,13 +38,22 @@ class OauthController < ApplicationController
 
   private
 
-  def set_consumer
-    @consumer = OAuth::Consumer.new("1b2197c12ae0e71d54a4", "afac7a8b9bc59b448d744cfbf3ac24e460749fcd", {
-      :site => "https://api.xing.com",
-      :request_token_path => "/v1/request_token",
-      :authorize_path => "/v1/authorize",
-      :access_token_path => "/v1/access_token"
-    })
-  end
+    def set_consumer
+      @consumer = OAuth::Consumer.new("1b2197c12ae0e71d54a4", "afac7a8b9bc59b448d744cfbf3ac24e460749fcd", {
+        :site => "https://api.xing.com",
+        :request_token_path => "/v1/request_token",
+        :authorize_path => "/v1/authorize",
+        :access_token_path => "/v1/access_token"
+      })
+    end
+
+    def callback_url
+      if Rails.env.production?
+        "http://rent-a-role-model.herokuapp.com/callback"
+       else
+        "http://localhost:3000/callback"
+      end
+    end
+
 
 end
