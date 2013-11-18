4b7e2780e4a9a57c8a00d2444e958e83860f90ce
lieblingswelt <lieblingswelt@posteo.de>
2013-11-18 19:49:29 +0100

    mail recipient is user

--- app/models/contact_form.rb
+++ app/models/contact_form.rb
@@ -1,11 +1,12 @@
 class ContactForm < MailForm::Base
   attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :email,      :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :file,      :attachment => true
 
   attribute :message
   attribute :nickname,  :captcha  => true
-  
+  attr_accessor :user 
+
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
@@ -13,7 +14,7 @@ class ContactForm < MailForm::Base
 def headers
     {
       :subject => "Rent a Role Model request",
-      :to => email,
+      :to => @user.email,
       :from => %("#{name}" <#{email}>)
     }
   end
