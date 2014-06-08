24e113c0e89c48200b8e180019cccddd838e8305
Stephanie Feddern <s.feddern@gmail.com>
2014-06-08 10:33:25 +0200

    added mailer and mail views. removed mail model. refactored form and form controller

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,23 +1,42 @@
+#encoding: utf-8
 class ContactFormsController < ApplicationController
+  #before_filter :valid_input, only: :create, except: :new
+  def new
+    @user = User.find(params[:user_id])
+  end 
 
-def new 
-	@user = User.find(params[:user_id])
-	@contact_form = ContactForm.new 
-end 
+  def create  
+    begin 
+      @user = User.find(params[:user_id])
+      #logger.debug("params in create #{params}")
+      if (request.post? && valid_input(params))
+        ContactFormMailer.contact_form_email(@user, params).deliver
+        render :create
+      else 
+        flash.now[:error] = 'Fehler beim Versenden. Bitte prüfen Sie Ihre Angaben.' 
+        render :new
+      end 
+    rescue ScriptError 
+       flash[:error] = 'Sorry, this message appears to be spam and was not delivered.' 
+    end
+  end
 
-def create	
-	begin 
-		@user = User.find(params[:user_id])
-		@contact_form = ContactForm.new(params[:contact_form]) 
-		@contact_form.user = @user
-		@contact_form.request = request 
-		if @contact_form.deliver 
-		else 
-			flash.now[:error] = 'Fehler beim Versenden. Bitte pruefen Sie Ihre Angaben.' 
-			render :new 
-		end 
-	rescue ScriptError 
-		flash[:error] = 'Sorry, this message appears to be spam and was not delivered.' 
-		end 
-	end 
-end 
+  private 
+  def valid_input params
+    #logger.debug("params #{params.inspect}")
+    valid = false
+    if request.post?
+      valid = (params[:name].present? && 
+      params[:message].present? && 
+      params[:school].present? && 
+      params[:event].present? && 
+      valid_email(params))
+      #logger.debug("valid: #{valid.class}")
+      return valid
+    end
+  end
+
+  def valid_email(params)
+    return (params[:email].present? && (params[:email] =~ /\A[\w\.%\+\-]+@[\w\-]+\.+[\w]{2,}\z/i))
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,11 +16,13 @@ RentARoleModel::Application.configure do
   # Don't care if the mailer can't send.
   #config.action_mailer.raise_delivery_errors = false
   #for development use mailcatcher, run $gem install mailcatcher
-  ActionMailer::Base.delivery_method = :smtp 
-  ActionMailer::Base.smtp_settings = { 
-  :address => "localhost", 
-  :port => 1025, 
-  :domain => " " } 
+  # ActionMailer::Base.delivery_method = :smtp 
+  # ActionMailer::Base.smtp_settings = { 
+  # :address => "localhost", 
+  # :port => 1025, 
+  # :domain => " " } 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
