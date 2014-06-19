b70ad79886b6bdee8d0758eefef01da59b617f20
Johannes Strampe <johannesstrampe@googlemail.com>
2014-06-19 22:08:31 +0200

    show only provided user information fields

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module RentARoleModel
   class Application < Rails::Application
     config.action_mailer.default_options = { :from => "rentarolemodel@gmail.com" }
     config.autoload_paths += %W(#{config.root}/lib)
+    config.i18n.default_locale = :de
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
